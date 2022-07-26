a874a71a9a07a096f82d73442e969b392056db06
ahmetson <ahmetson@zoho.com>
2022-07-27 02:25:27 +0300

    add LORD contract

--- contracts/erc20/Lord.sol
+++ contracts/erc20/Lord.sol
@@ -1,56 +1,127 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "./Lottery.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- *  @title Blocklords Onsales
- *  @author Medet Ahmetson (ahmetson@zoho.com)
- *  @notice This contract is for tracking
+ *  @title Blocklords
+ *  @author Medet Ahmetson (admin@blocklords.io)
+ *  @notice The LORD token
  */
-contract Lord is Lottery {
-    mapping (address => uint) public amounts;
-    IERC721 old;
+contract Lord is ERC20, Ownable {
+    /// @notice Set to false to stop mint/burn of token. Set to true to allow minting.
+    bool public bridgeAllowed = false;
 
-    event Buy(address indexed buyer, uint price, uint amount);
-    event BurnOldNft(address indexed owner, uint indexed tokenId);
+    /// @notice the list of bridge addresses allowed to mint tokens.
+    mapping(address => bool) public bridges;
 
-    constructor (address _old, address _verifier, address _fund) Lottery (_verifier, _fund) {
-        old = IERC721(_old);
+    // Mint and Burn
+    modifier onlyBridge {
+        require(bridgeAllowed && bridges[msg.sender]);
+        _;
     }
 
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // User functions.
-    //
-    ////////////////////////////////////////////////////////////////////////////
+    event AddBridge(address indexed bridge);
+    event RemoveBridge(address indexed bridge);
 
-    /// @notice Buy a bronze silver pack.
-    /// @param price is the price that user has to pay
-    function buy(uint price, uint8 v, bytes32 r, bytes32 s) external payable {
-        require(price > 0, "PRICE_0");
-        require(msg.value == price, "INVALID_ATTACHED_ETH");
+    /// @param _bridgeAllowed is FALSE in the token at the original token.
+    constructor(
+        address _seedSale,
+        address _strategicSale,
+        address _privateSale,
+        address _launchpads,
+        address _ieo,
+        address _lordsBounty,
+        address _kingsBounty,
+        address _dynastyIncentives,
+        address _liquidity,
+        address _foundationReserve,
+        address _advisor,
+        bool _bridgeAllowed) ERC20("BLOCKLORDS", "LORD") {
+        bridgeAllowed = _bridgeAllowed;
+        uint256 _million = 1000 * 1000 * 10 ** 18;
+        uint256 thousand = 1000 * 10 ** 18;
 
-        amounts[msg.sender]++;
+        if (!_bridgeAllowed) {
+            _mint(_seedSale, 8 * _million + (750 * thousand));  // 8.75% of 100 million
+            _mint(_seedSale, 6 * _million + (250 * thousand));  // 8.75% of 100 million
+            _mint(_privateSale, 7 * _million);  // 8.75% of 100 million
+            _mint(_launchpads, 2 * _million);  // 8.75% of 100 million
+            _mint(_ieo, 1 * _million);  // 8.75% of 100 million
+            _mint(_lordsBounty, 25 * _million);  // 8.75% of 100 million
+            _mint(_kingsBounty, 10 * _million);  // 8.75% of 100 million
+            _mint(_dynastyIncentives, 15 * _million);  // 8.75% of 100 million
+            _mint(_liquidity, 10 * _million);  // 8.75% of 100 million
+            _mint(_foundationReserve, 10 * _million);  // 8.75% of 100 million
+            _mint(_advisor, 5 * _million);  // 8.75% of 100 million
 
-        // investor, level verification with claim verifier
-	    bytes memory prefix     = "\x19Ethereum Signed Message:\n32";
-	    bytes32 message         = keccak256(abi.encodePacked(msg.sender, address(this), block.chainid, amounts[msg.sender]));
-	    bytes32 hash            = keccak256(abi.encodePacked(prefix, message));
-	    address recover         = ecrecover(hash, v, r, s);
-	    require(recover == verifier,                   "SIG");
-
-        payable(fund).transfer(msg.value);
-
-        emit Buy(msg.sender, msg.value, amounts[msg.sender]);
+            require(_totalSupple == 100 * _million, "not a 100 million tokens");
+        }
     }
 
+    function addBridge(address _bridge) external onlyOwner {
+        require(bridgeAllowed, "no bridging");
+        require(_bridge != address(0), "invalid address");
+        require(!bridges[_bridge], "bridge already added");
 
-    function burnOld(uint nftId) external {
-        require (old.isApprovedForAll(msg.sender, address(this)) || old.getApproved(nftId) == address(this), "NOT_APPROVED");
-        old.safeTransferFrom(msg.sender, 0x000000000000000000000000000000000000dEaD, nftId);
+        bridges[_bridge] = true;
 
-        emit BurnOldNft(msg.sender, nftId);
+        emit AddBridge(_bridge);
+   }
+
+    function removeBridge(address _bridge) external onlyOwner {
+        require(bridgeAllowed, "no bridging");
+        require(_bridge != address(0), "invalid address");
+        require(bridges[_bridge], "bridge already removed");
+
+        delete bridges[_bridge];
+
+        emit RemoveBridge(_bridge);
+   }
+
+   /**
+     * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) external onlyBridge {
+        require(_totalSupply.add(amount) <= limitSupply, "exceeded mint limit");
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     *
+     * Included just to follow the standard of OpenZeppelin.
+     */
+    function burn(uint256 amount) public {
+        require(false, "Only burnFrom is allowed");
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public onlyBridge {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "burn amount exceeds allowance");
+
+        _approve(account, _msgSender(), currentAllowance
+            .sub(amount, "transfer amount exceeds allowance"));
+        _burn(account, amount);
     }
 }
