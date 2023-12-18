6b89f0d0325e7ea261bfd810781a3bdb2ff6a038
seasonlei <30384084@qq.com>
2023-12-18 13:14:02 +0800

    the file path is changed

--- contracts/LRD/LRDClaim.sol
+++ contracts/LRD/LRDClaim.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LrdGame is Ownable {
+contract LRDClaim is Ownable {
 
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
