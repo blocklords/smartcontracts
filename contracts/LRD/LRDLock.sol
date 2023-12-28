08bcb1cf6b4281cd290a798b367cd425e8b9b78c
seasonlei <30384084@qq.com>
2023-12-28 20:43:46 +0800

    add getAllUserInfo method Perform revenue calculations for the back end

--- contracts/LRD/LRDLock.sol
+++ contracts/LRD/LRDLock.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LRDLock is Ownable {
 
-    using SafeMath for uint256;
+    using SafeMath  for uint256;
     using SafeERC20 for IERC20;
 
     struct PlayerParams {
@@ -17,20 +17,24 @@ contract LRDLock is Ownable {
         uint256 importType;
     }
 
-    struct PlayerInfo {
-        address walletAddress;
-        PlayerParams params;
+    struct UserData {
+        uint256 amount;      // The amount deposited this time
+        uint256 importTime;  // The time of thie deposit
+        uint256 totalAmount; // Total deposits (as of current deposits)
+        uint256 endTime;     // Maturity time
+        uint256 importType;  // The type of this deposit
     }
 
-    bool public gameStatus;
-    address public verifier;
-    address public lrd;
+    bool      public gameStatus;
+    address   public verifier;
+    address   public lrd;
     address[] public playerList;
     uint256[] public importTypes;
-    uint256 public totalAmount;
-    uint256 public activeUsers;
+    uint256   public totalAmount;
+    uint256   public activeUsers;
 
     mapping(address => PlayerParams) public player;
+    mapping(address => UserData[]) public usersData;
     mapping(address => bool) public addressExists;
     mapping(address => uint256) public nonce;
 
@@ -41,14 +45,14 @@ contract LRDLock is Ownable {
 
     constructor(address _verifier, address _token) {
         require(_verifier != address(0), "Lrd: Address error");
-        require(_token != address(0), "Lrd: Address error");
+        require(_token    != address(0), "Lrd: Address error");
+
+        verifier    = _verifier;
+        lrd         = _token;
+        gameStatus  = true;
 
         // Add importType slices for the seconds of 0 days, 1 week, 1 month, 6 months, 1 year, and 4 years, respectively
         importTypes = [0, 86400 * 7, 86400 * 30, 86400 * 180, 86400 * 365, 86400 * 365 * 4];
-
-        verifier = _verifier;
-        lrd = _token;
-        gameStatus = true;
     }
 
     function importLrd(uint256 _amount, uint256 _importType, uint8 _v, bytes32 _r, bytes32 _s) external {
@@ -76,16 +80,26 @@ contract LRDLock is Ownable {
         if(!addressExists[msg.sender]){
             playerList.push(msg.sender);
             addressExists[msg.sender] = true;
-            activeUsers += 1;
+            activeUsers              += 1;
         }
 
         IERC20 _token = IERC20(lrd);
         require(_token.balanceOf(msg.sender) >= _amount, "Lrd: Not enough token to import");
         _token.safeTransferFrom(msg.sender, address(this), _amount);
 
-        params.amount += _amount;
+        params.amount    += _amount;
         params.importType = _importType;
-        totalAmount += _amount;
+        totalAmount      += _amount;
+
+        // Push in current wallet pledge information for easy back-end calculation of returns
+        UserData memory userData;
+        userData.amount      = _amount;
+        userData.importTime  = block.timestamp;
+        userData.totalAmount = params.amount;
+        userData.endTime     = params.importTime + importTypes[_importType];
+        userData.importType  = _importType;
+        usersData[msg.sender].push(userData);
+
         nonce[msg.sender]++;
 
         emit ImportLrd(msg.sender, _amount, _importType, checkTime(params), block.timestamp);
@@ -109,13 +123,14 @@ contract LRDLock is Ownable {
         require(_token.balanceOf(address(this)) >= params.amount, "Lrd: There is not enough balance to export");
         _token.safeTransfer(msg.sender, params.amount);
 
-        totalAmount -= params.amount;
-        params.amount = 0;
-        params.importTime = 0;
-        params.importType = 0;
+        totalAmount              -= params.amount;
+        params.amount             = 0;
+        params.importTime         = 0;
+        params.importType         = 0;
         addressExists[msg.sender] = false;
-        activeUsers -= 1;
+        activeUsers              -= 1;
         nonce[msg.sender]++;
+        delete usersData[msg.sender];
 
         emit ExportLrd(msg.sender, params.amount, block.timestamp);
 
@@ -130,18 +145,71 @@ contract LRDLock is Ownable {
     }
 
     // Get all player import information
-    function getAllUser() external view returns (PlayerInfo[] memory) {
+    function getAllUserInfo() external view returns (string memory) {
         uint256 userCount = playerList.length;
-        PlayerInfo[] memory allUserInfo = new PlayerInfo[](userCount);
+        string memory result;
 
         for (uint256 i = 0; i < userCount; i++) {
             address userAddress = playerList[i];
-            PlayerParams memory params = player[userAddress];
-            PlayerInfo memory userInfo = PlayerInfo(userAddress, params);
-            allUserInfo[i] = userInfo;
+            UserData[] storage user = usersData[userAddress];
+
+            result = string(abi.encodePacked(result, userAddressToString(userAddress), ":"));
+
+            for (uint256 j = 0; j < user.length; j++) {
+                result = string(abi.encodePacked(result, depositDetailToString(user[j])));
+
+                if (j < user.length - 1) {
+                    result = string(abi.encodePacked(result, ";"));
+                }
+            }
+
+            if (i < userCount - 1) {
+                result = string(abi.encodePacked(result, "@@"));
+            }
         }
 
-        return allUserInfo;
+        return result;
+    }
+
+    // Converts the user address to a string
+    function userAddressToString(address _address) public pure returns (string memory) {
+        uint256 addressUint = uint256(uint160(_address));
+        return toString(addressUint);
+    }
+
+    // Convert DepositDetail to a string
+    function depositDetailToString(UserData memory _detail) internal pure returns (string memory) {
+        string memory amountStr = toString(_detail.amount);
+        string memory importTimeStr = toString(_detail.importTime);
+        string memory importTypeStr = toString(_detail.importType);
+        string memory totalAmountStr = toString(_detail.totalAmount);
+        string memory endTimeStr = toString(_detail.endTime);
+
+        return string(abi.encodePacked(amountStr, ",", importTimeStr, ",", importTypeStr, ",", totalAmountStr, ",", endTimeStr));
+}
+
+    // Convert bytes32 to a string
+    function toString(uint256 _data) internal pure returns (string memory) {
+        if (_data == 0) {
+            return "0";
+        }
+
+        uint256 temp = _data;
+        uint256 digits;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        bytes memory buffer = new bytes(digits);
+        while (_data != 0) {
+            digits        -= 1;
+            buffer[digits] = bytes1(uint8(48 + _data % 10));
+            _data         /= 10;
+        }
+
+        return string(buffer);
     }
     
     // Change the checkout wallet
